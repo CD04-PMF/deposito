75e529abbf7db1f7a2aaf3af5e63f6de8cde46d3
Pedro MF <ue216731@edu.xunta.gal>
2022-03-08 16:31:23 +0100

    Guardo cambios Main.java

--- deposito/src/main/java/cuentas/Main.java
+++ deposito/src/main/java/cuentas/Main.java
@@ -7,11 +7,11 @@ public class Main {
         
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         
-        float cantidad;
-        operativa_cuenta(cuenta1, cantidad);
+        
+        operativa_cuenta(cuenta1);
     }
 
-    private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+    private static void operativa_cuenta(CCuenta cuenta1) {
         
         double saldoActual;
         saldoActual = cuenta1.estado();
